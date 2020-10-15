33a75b56bcfe6cc8585e00b30c255c88c2baae47
Destino56 <alvmarigl56@gmail.com>
2020-10-15 15:39:25 +0200

    Creo que ahora si que va

--- src/paquete/HiloNumerosPrimos.java
+++ src/paquete/HiloNumerosPrimos.java
@@ -2,7 +2,7 @@ package paquete;
 
 import java.util.Scanner;
 
-public class HiloNumerosPrimos extends Thread{
+public class HiloNumerosPrimos implements Runnable{
 	
 	int inicio;
 	int tope;
@@ -18,17 +18,17 @@ public class HiloNumerosPrimos extends Thread{
 	}
 	
 	private void calculaNumPrimos(int inicio, int tope) {
-		//BigInteger bigInteger = null; //He probado con el isProbablyPrime pero me termina de funcionar, así que lo he hecho marranero
-										//para ver si al menos va
+		//BigInteger bigInteger = null; 
+		int cont=0;
 		for(int i=inicio ; i<tope ; i++) {
 			/*if(bigInteger.isProbablePrime(i)) {
 				System.out.print(i+" ");
 			}*/
 			if(i%2==0 || i%3==0 || i%5==0 || i%7==0 || i%11==0 || i%13==0 || i%17==0 || i%19==0 || i%23==0 || i%29==0) {			
 			}else {
-				System.out.print(i+" ");
+				System.out.println(i);
+				cont++;
 			}
-			//teclado.nextLine();
 		}
 	}
 }
